e9e579a8b0fd65b000dc2ccd42b132aec4210fb4
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-01-09 22:34:45 -0300

    agrega componente footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div>
+      <h4>Imágenes de planetas</h4>
+      <button>Botón a confirmar si va acá</button>
+    </div>
+  );
+};
+
+export default Footer;
